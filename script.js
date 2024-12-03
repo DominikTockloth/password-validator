e541c684ac72f1de2166d82a753c18f0f0241a13
DominikTockloth <128898734+DominikTockloth@users.noreply.github.com>
2024-12-03 09:57:41 +0100

    added progressbar function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let placeholder = document.getElementById('placeholder');
 let input = document.getElementById('input');
 let password = document.getElementById('input').value;
 let progressBar = document.getElementById('progress-bar');
+let toggleButton = document.getElementById('toggleBtn');
 
 function validatePassword(password) {
     // Checking for conditions
@@ -26,25 +27,72 @@ function updateImage(criteria, isValid) {
     }
 }
 
-function resetImages() {
-    // Reset all pics
-    const images = document.querySelectorAll('.content img');
-    images.forEach(image => {
-        image.src = 'img/cancel.png';
-    });
+// Updates progress bar in width and backgroundcolor
+function updateProgressBar(password) {
+    let progress = 0;
+    let color = 'rgb(180, 180, 180)';
+    if (/[a-z]/.test(password) && /[A-Z]/.test(password)) {
+        progress += 40;
+    }
+    if (/[0-9]/.test(password)) {
+        progress += 20;
+    }
+    if (/[!@?&$#%]/.test(password)) {
+        progress += 20;
+    }
+    if (password.length >= 8) {
+        progress += 20;
+    }
+    if (progress === 100) {
+        color = 'lightgreen';
+    }
+    else if (progress >= 80) {
+        color = 'yellow';
+    } else if (progress >= 60) {
+        color = 'orange';
+    } else if (progress >= 40) {
+        color = 'red';
+    } else {
+        color = 'rgb(180, 180, 180)'; 
+    }
+    progressBar.style.width = `${progress}%`;
+    progressBar.style.backgroundColor = color;
 }
 
+// Resets the check images, when input is empty
+function resetImages() {
+    if (password === '') {
+        const images = document.querySelectorAll('.content img');
+        images.forEach(image => {
+            image.src = 'img/cancel.png';
+        });
+    }
+
+}
+
+// Toggle password visibility
+function togglePasswordVisibility() {
+    if (input.type === 'password') {
+        input.type = 'text';
+        toggleButton.src = 'img/not-visible.png';
+    } else {
+        input.type = 'password';
+        toggleButton.src = 'img/visible.png';
+    }
+}
+
+// Eventlistener for input , updating the progressbar
+document.getElementById("input").addEventListener("input", (event) => {
+    updateProgressBar(event.target.value);
+});
 
 // EventListener for input if onFocus or onBlur and changes the placeholder class
-
 input.addEventListener("focus", () => {
-    placeholder.classList.add('onFocus');
     input.style.borderColor = 'rgb(173, 102, 241)';
 });
 
 input.addEventListener("blur", () => {
-    placeholder.classList.remove('onFocus');
     input.style.borderColor = 'rgb(28, 28, 28)';
-    input.value = '';
-    resetImages();
 });
+
+
